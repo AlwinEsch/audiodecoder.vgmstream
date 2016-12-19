39967a7001875b196aa6b559897c2bca00509929
Alwin Esch <alwin.esch@web.de>
2016-12-19 18:24:05 +0100

    add initial functions for instance handling (currently unused)

--- src/VGMCodec.cpp
+++ src/VGMCodec.cpp
@@ -107,6 +107,15 @@ void ADDON_Announce(const char *flag, const char *sender, const char *message, c
 {
 }
 
+ADDON_STATUS ADDON_CreateInstance(int instanceType, const char* instanceID, const void* instanceProps, void* instanceFunctions, void* kodiInstance, void** addonInstance)
+{
+  return ADDON_STATUS_UNKNOWN;
+}
+
+void ADDON_DestroyInstance(int instanceType, const char* instanceID, void* instance)
+{
+}
+
 struct VGMContext
 {
   STREAMFILE sf;
